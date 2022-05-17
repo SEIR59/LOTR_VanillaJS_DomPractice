a0022176d121eca14d01416054232b163be31527
andersons1218 <andersons1218@gmail.com>
2022-05-17 17:43:50 -0400

    LOTR chapter 1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 console.log('Linked.')
-
+const body = document.querySelector('body')
 // Dramatis Personae
 const hobbits = [
 	`Frodo Baggins`,
@@ -20,22 +20,21 @@ const lands = ['The-Shire', 'Rivendell', 'Mordor']
 
 function makeMiddleEarth() {
 	console.log('1: makeMiddleEarth')
+	
+	const middleEarthEL = document.createElement("section");
+	middleEarthEL.setAttribute("id", "middle-earth") 
+	lands.forEach( land => {
+	const landEL = document.createElement("article")
+	landEL.setAttribute("id", `${land}`)
+	const landTitleTag = document.createElement("h1")
+	landTitleTag.innerHtml =`${land}`
+	landEL.appendChild(landTitleTag)
+	middleEarthEL.appendChild(landEL)
+	})
 
-	// 1. create a section tag with an id of middle-earth
-
-	// 2. use a for loop to iterate over the lands array that does the following:
-
-	//   2a. creates an article tag (there should be one for each land when the loop is done)
-
-	//   2b. gives each land article an `id` tag of the corresponding land name
-
-	//   2c. includes an h1 with the name of the land inside each land article
-
-	//   2d. appends each land to the middle-earth section
-
-	// 3. append the section to the body of the DOM.
+	body.appendChild(middleEarthEL)
+	console.log(middleEarthEL)
 }
-
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 1 complete - Made Middle Earth".
 
@@ -264,3 +263,4 @@ document.getElementById('all-chapters').addEventListener('click', () => {
 	weWantsIt()
 	thereAndBackAgain()
 })
+
